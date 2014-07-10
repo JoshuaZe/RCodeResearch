0ca3900c29af68e54e4b329d4e967a84257ee6de
JoshuaZe <joshua_zhang@foxmail.com>
2014-07-10 23:39:21 +0800

    test for independence,Correspondene Analysis

--- longshang_weather_channel_test.R
+++ longshang_weather_channel_test.R
@@ -87,6 +87,7 @@ barplot(beside=TRUE,
         )
 par(mar = c(0, 0, 0, 0))
 pie(cases,col=rainbow(4),radius = 1)
+summary(fdata$C2.34)
 #####
 # C2.35
 #####
@@ -104,6 +105,8 @@ barplot(beside=TRUE,
         density = c(7.5,12.5,17.5,22.5),
         angle = c(45,60,120,135)
 )
+chisq.test(cases[1:2])
+binom.test(cases[3],550,p=0.5,alternative="less")
 #check if C2.35_88==1 but C2.35_1_2_3!=1
 which(data[which(data$C2.35_88==1),2:4]!=c(0,0,0))
 #check if all zero
@@ -113,7 +116,7 @@ cases <- c(length(which(rowSums(data[,2:4])==1)),
            length(which(rowSums(data[,2:4])==2)),
            length(which(rowSums(data[,2:4])==3)),
            length(which(rowSums(data[,2:4])==0)))
-names(cases) <- c("1","2","3","0")
+names(cases) <- c("留意1个变化","留意2个变化","留意3个变化","没留意变化")
 par(mar = c(3, 3, 3, 3))
 barplot(beside=TRUE,
         cases,
@@ -129,9 +132,9 @@ choose_2 <- which(data$C2.35_2==1)
 choose_3 <- which(data$C2.35_3==1)
 choose_88 <- which(data$C2.35_88==1)
 venn.plot <- venn.diagram(
-  x = list("1"=choose_1,
-           "2"=choose_2,
-           "3"=choose_3),
+  x = list("变化1"=choose_1,
+           "变化2"=choose_2,
+           "变化3"=choose_3),
   fill=c("red","blue","green"),
   filename = "C2.35_Venn.tiff",
   scaled = TRUE,
@@ -174,7 +177,7 @@ cases <- c(length(which(rowSums(data[,6:9])==1)),
            length(which(rowSums(data[,6:9])==2)),
            length(which(rowSums(data[,6:9])==3)),
            length(which(rowSums(data[,6:9])==4)))
-names(cases) <- c("1","2","3","4")
+names(cases) <- c("喜欢1点(403)","喜欢2点(139)","喜欢3点(10)","喜欢4点(1)")
 par(mar = c(3, 3, 3, 3))
 barplot(beside=TRUE,
         cases,
@@ -190,10 +193,10 @@ choose_2 <- which(data$C2.36_2==1)
 choose_3 <- which(data$C2.36_3==1)
 choose_4 <- which(data$C2.36_4==1)
 venn.plot <- venn.diagram(
-  x = list("1"=choose_1,
-           "2"=choose_2,
-           "3"=choose_3,
-           "4"=choose_4),
+  x = list("方面1"=choose_1,
+           "方面2"=choose_2,
+           "方面3"=choose_3,
+           "方面4"=choose_4),
   fill=c("red","blue","green","grey"),
   filename = "C2.36_Venn.tiff",
   scaled = TRUE,
@@ -267,13 +270,48 @@ barplot(beside=TRUE,
         angle = c(45,60,120,135,150)
 )
 #####
+# C2.37_38_39
+#####
+cases37 <- c(length(which(data$C2.37==1)),
+           length(which(data$C2.37==2)),
+           length(which(data$C2.37==3)),
+           length(which(data$C2.37==4)),
+           length(which(data$C2.37==5)))
+
+cases38 <- c(length(which(data$C2.38==1)),
+           length(which(data$C2.38==2)),
+           length(which(data$C2.38==3)),
+           length(which(data$C2.38==4)),
+           length(which(data$C2.38==5)))
+
+cases39 <- c(length(which(data$C2.39==1)),
+           length(which(data$C2.39==2)),
+           length(which(data$C2.39==3)),
+           length(which(data$C2.39==4)),
+           length(which(data$C2.39==5)))
+cases<-rbind(cases37,cases38,cases39);
+colnames(cases)=c("很不满意","不太满意","一般","比较满意","非常满意")
+rownames(cases)=c("谈笑","策巴子","憨妹")
+cases
+# plot grouped bar by using barplot
+barplot(cases,
+        beside=T,
+        cex.names=0.8,
+        #las=2,
+        legend.text = T,
+        border="black",
+        col=c("green3","blue","red"),
+        density = c(7.5,12.5,17.5),
+        angle = c(60,120,150)
+)
+#####
 # C2.40
 #####
 cases <- c(length(which(data$C2.40_1==1)),
            length(which(data$C2.40_2==1)),
            length(which(data$C2.40_3==1)),
            length(which(data$C2.40_88==1)))
-names(cases) <- c("1","2","3","88")
+names(cases) <- c("央视","地方台","湖北地面频道","都不看")
 #barplot(cases)
 par(mar = c(3, 3, 3, 3))
 barplot(beside=TRUE,
@@ -292,7 +330,7 @@ cases <- c(length(which(rowSums(data[,13:15])==1)),
            length(which(rowSums(data[,13:15])==2)),
            length(which(rowSums(data[,13:15])==3)),
            length(which(rowSums(data[,13:15])==0)))
-names(cases) <- c("1","2","3","0")
+names(cases) <- c("还看1台","还看2台","还看3台","不看")
 par(mar = c(3, 3, 3, 3))
 barplot(beside=TRUE,
         cases,
@@ -308,9 +346,9 @@ watch_2 <- which(data$C2.40_2==1)
 watch_3 <- which(data$C2.40_3==1)
 watch_88 <- which(data$C2.40_88==1)
 venn.plot <- venn.diagram(
-  x = list("1"=watch_1,
-           "2"=watch_2,
-           "3"=watch_3),
+  x = list("央视"=watch_1,
+           "地方台"=watch_2,
+           "湖北地面频道"=watch_3),
   fill=c("red","blue","green"),
   filename = "C2.40_Venn.tiff",
   scaled = TRUE,
@@ -337,30 +375,31 @@ venn.plot
 # Correspondence analysis
 # C2.34与C2.35_1_2_3_88
 #####
-table(fdata$C2.34,factor(rowSums(data[,2:4])))
-summary(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
+x<-table(fdata$C2.34,factor(rowSums(data[,2:4])))
+summary(x)
+x
+colnames(x)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x)<-c("06:12","11:40","21:00","20:15")
+x
 #chisq.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
 #fisher.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
 #mcnemar.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))),correct=FALSE)
 #Correspondence analysis
 library(ca)
-ca(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
-plot(ca(table(fdata$C2.34,factor(rowSums(data[,2:4])))))
+ca(x)
+plot(ca(x), dim = c(1,2), map = "symbiplot", what = c("all", "all"), 
+     mass = c(FALSE, FALSE), contrib = c("none", "none"), 
+     col = c("blue", "red"), pch = c(16, 21, 17, 24), 
+     labels = c(2, 2), arrows = c(FALSE, FALSE),
+     lines = c(FALSE, FALSE), xlab = "_auto_", ylab = "_auto_",
+     col.lab = c("blue", "red")) 
 #####
 # test for independence
 # Correspondence analysis
 # C2.36_1_2_3_4与C2.35_1_2_3_88
 #####
-#analysis on the amount of likes
-table(factor(rowSums(data[,6:9])),factor(rowSums(data[,2:4])))
-summary(table(factor(rowSums(data[,6:9])),factor(rowSums(data[,2:4]))))
-#chisq.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
-#fisher.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))))
-#mcnemar.test(table(fdata$C2.34,factor(rowSums(data[,2:4]))),correct=FALSE)
 #Correspondence analysis
 library(ca)
-ca(table(factor(rowSums(data[,6:9])),factor(rowSums(data[,2:4]))))
-plot(ca(table(factor(rowSums(data[,6:9])),factor(rowSums(data[,2:4])))))
 #analysis on the types of likes
 x1<-table(fdata$C2.36_1,factor(rowSums(data[,2:4])))
 x1
@@ -380,8 +419,173 @@ x[2,]<-x2[2,]/(x2[1,]+x2[2,])
 x[3,]<-x3[2,]/(x3[1,]+x3[2,])
 x[4,]<-x4[2,]/(x4[1,]+x4[2,])
 x<-as.table(x)
-colnames(x)<-c("0","1","2","3")
-row.names(x)<-c("1","2","3","4")
+colnames(x)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x)<-c("(1)娱乐化","(2)介绍生活生产小常识",
+                "(3)服务生活指导生产","(4)节目互动")
 summary(x)
 x
 plot(ca(x))
+#####
+# test for independence
+# Correspondence analysis
+# C2.40_1_2_3_88与C2.35_1_2_3_88
+#####
+#Correspondence analysis
+library(ca)
+#analysis on the types of likes
+x1<-table(fdata$C2.40_1,factor(rowSums(data[,2:4])))
+x1
+summary(x1)
+x2<-table(fdata$C2.40_2,factor(rowSums(data[,2:4])))
+x2
+summary(x2)
+x3<-table(fdata$C2.40_3,factor(rowSums(data[,2:4])))
+x3
+summary(x3)
+x4<-table(fdata$C2.40_88,factor(rowSums(data[,2:4])))
+x4
+summary(x4)
+x<-matrix(1,4,4)
+x[1,]<-x1[2,]/(x1[1,]+x1[2,])
+x[2,]<-x2[2,]/(x2[1,]+x2[2,])
+x[3,]<-x3[2,]/(x3[1,]+x3[2,])
+x[4,]<-x4[2,]/(x4[1,]+x4[2,])
+x<-as.table(x)
+colnames(x)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x)<-c("央视","地方台",
+                "湖北地面频道","只看垄上气象台")
+summary(x)
+x
+plot(ca(x))
+#####
+# test for independence
+# Correspondence analysis
+# C2.37-38-39与C2.35_1_2_3_88
+#####
+#Correspondence analysis
+library(ca)
+#analysis on the types of likes
+x1<-table(fdata$C2.37,factor(rowSums(data[,2:4])))
+x1
+summary(x1)
+colnames(x1)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x1)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x1)
+x1
+plot(ca(x1[3:5,]))
+
+x2<-table(fdata$C2.38,factor(rowSums(data[,2:4])))
+x2
+summary(x2)
+colnames(x2)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x2)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x2)
+x2
+plot(ca(x2[3:5,]))
+
+x3<-table(fdata$C2.39,factor(rowSums(data[,2:4])))
+x3
+summary(x3)
+colnames(x3)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x3)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x3)
+x3
+plot(ca(x3[3:5,]))
+#each holder
+ex<-matrix(1,5,4)
+ex[,1]<-x1[,1]/sum(x1[,1])
+ex[,2]<-x1[,2]/sum(x1[,2])
+ex[,3]<-x1[,3]/sum(x1[,3])
+ex[,4]<-x1[,4]/sum(x1[,4])
+ex1<-colSums(ex*c(1,2,3,4,5))
+
+ex<-matrix(1,5,4)
+ex[,1]<-x2[,1]/sum(x2[,1])
+ex[,2]<-x2[,2]/sum(x2[,2])
+ex[,3]<-x2[,3]/sum(x2[,3])
+ex[,4]<-x2[,4]/sum(x2[,4])
+ex2<-colSums(ex*c(1,2,3,4,5))
+
+ex<-matrix(1,5,4)
+ex[,1]<-x3[,1]/sum(x3[,1])
+ex[,2]<-x3[,2]/sum(x3[,2])
+ex[,3]<-x3[,3]/sum(x3[,3])
+ex[,4]<-x3[,4]/sum(x3[,4])
+ex3<-colSums(ex*c(1,2,3,4,5))
+
+x<-matrix(1,3,4)
+x[1,]<-ex1
+x[2,]<-ex2
+x[3,]<-ex3
+x<-as.table(x)
+colnames(x)<-c("没留意变化","留意1个","留意2个","留意3个")
+row.names(x)<-c("谈笑","策巴子","憨妹")
+summary(x)
+x
+plot(ca(x))
+#####
+# test for independence
+# Correspondence analysis
+# C2.37-38-39与C2.34
+#####
+#Correspondence analysis
+library(ca)
+#analysis on the types of likes
+x1<-table(fdata$C2.37,fdata$C2.34)
+x1
+colnames(x1)<-c("06:12","11:40","21:00","20:15")
+row.names(x1)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x1)
+x1
+
+x2<-table(fdata$C2.38,fdata$C2.34)
+x2
+colnames(x2)<-c("06:12","11:40","21:00","20:15")
+row.names(x2)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x2)
+x2
+
+x3<-table(fdata$C2.39,fdata$C2.34)
+x3
+colnames(x3)<-c("06:12","11:40","21:00","20:15")
+row.names(x3)<-c("很不满意","不太满意","一般","比较满意","非常满意")
+summary(x3)
+x3
+#each holder
+ex<-matrix(1,5,4)
+ex[,1]<-x1[,1]/sum(x1[,1])
+ex[,2]<-x1[,2]/sum(x1[,2])
+ex[,3]<-x1[,3]/sum(x1[,3])
+ex[,4]<-x1[,4]/sum(x1[,4])
+ex1<-colSums(ex*c(1,2,3,4,5))
+
+ex<-matrix(1,5,4)
+ex[,1]<-x2[,1]/sum(x2[,1])
+ex[,2]<-x2[,2]/sum(x2[,2])
+ex[,3]<-x2[,3]/sum(x2[,3])
+ex[,4]<-x2[,4]/sum(x2[,4])
+ex2<-colSums(ex*c(1,2,3,4,5))
+
+ex<-matrix(1,5,4)
+ex[,1]<-x3[,1]/sum(x3[,1])
+ex[,2]<-x3[,2]/sum(x3[,2])
+ex[,3]<-x3[,3]/sum(x3[,3])
+ex[,4]<-x3[,4]/sum(x3[,4])
+ex3<-colSums(ex*c(1,2,3,4,5))
+
+x<-matrix(1,3,4)
+x[1,]<-ex1
+x[2,]<-ex2
+x[3,]<-ex3
+x<-as.table(x)
+x
+colnames(x)<-c("06:12","11:40","21:00","20:15")
+row.names(x)<-c("谈笑","策巴子","憨妹")
+summary(x)
+x
+plot(ca(x), dim = c(1,2), map = "symmetric", what = c("all", "all"), 
+     mass = c(FALSE, FALSE), contrib = c("none", "none"), 
+     col = c("blue", "red"), pch = c(16, 21, 17, 24), 
+     labels = c(2, 2), arrows = c(FALSE, FALSE),
+     lines = c(FALSE, FALSE), xlab = "_auto_", ylab = "_auto_",
+     col.lab = c("blue", "red"))
