a1ceed3e9f20b53a7798014986e1462d30126da9
JoshuaZe <joshua_zhang@foxmail.com>
2014-12-01 01:00:21 +0800

    随机森林

---
+++ randomForest.R
@@ -0,0 +1,16 @@
+#http://cran.rstudio.com/web/views/MachineLearning.html
+#http://cran.rstudio.com/web/views/Optimization.html
+#http://cran.rstudio.com/web/views/NumericalMathematics.html
+#http://cran.rstudio.com/web/views/HighPerformanceComputing.html
+#http://cran.rstudio.com/web/views/NaturalLanguageProcessing.html
+
+#http://cran.rstudio.com/web/views/ReproducibleResearch.html
+#http://cran.rstudio.com/web/views/OfficialStatistics.html
+
+#http://blog.sina.com.cn/s/blog_62b37bfe0101hom5.html
+#####
+#Breiman and Cutler's random forests for classification and regression
+#####
+library(randomForest)
+
+#http://cran.rstudio.com/web/packages/googleVis/index.html
